9db2b250a76d6af6a45d1dff03894d19e11c6d5e
kristinaqualben <60709134+kristinaqualben@users.noreply.github.com>
2025-02-17 14:53:43 -0500

    create ModelConfig

--- src/configs.py
+++ src/configs.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
+from typing import Type
 
 
 @dataclass
@@ -7,3 +8,19 @@ class TrainConfig:
     batches: int
     learning_rate: float
     save_fig: bool
+
+@dataclass
+class ModelConfig:
+    epochs: int
+    batches: int
+    learning_rate: float
+    num_classes: int
+    input_n: int
+    hidden_n: int
+
+    @classmethod
+    def from_dict(cls, **kwargs):
+        # valid_fields = {f.name for f in fields(cls)}
+        filtered = {k: v for k, v in kwargs.items() if k in {f.name for f in fields(cls)}}
+        return cls(**filtered)
+    
