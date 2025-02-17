e6447cbdcf7cbf9edbc4e9c1f4683245c14c883a
kristinaqualben <60709134+kristinaqualben@users.noreply.github.com>
2025-02-17 12:31:17 -0500

    Update configs.py
    drop unused dataclass

--- src/configs.py
+++ src/configs.py
@@ -1,15 +1,4 @@
 from dataclasses import dataclass
-from typing import Optional
-from torch.utils.data import Dataset
-
-
-@dataclass
-class DataLoaderConfig:
-    dataset: Dataset
-    batch : int
-    test_size : float = 0.7 
-    shuffle : Optional[bool] = None
-    drop_last: Optional[bool] = None
 
 @dataclass
 class TrainConfig:
